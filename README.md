e2ad313b9edb01f327321ee035d8858565166f5c
Nesrine BOUZAZI <bouzazinesrine23@gmail.com>
2026-02-28 11:04:22 +0100

    Configuration pour le déploiement : ajout Procfile et gestion dynamique du port

---
+++ README.md
@@ -0,0 +1,5 @@
+1- pip install -r requirements.txt
+
+2- Créer un .env avec les clés API.
+
+3- python main.py
--- main.py
+++ main.py
@@ -169,4 +169,6 @@ async def text_to_speech(background_tasks: BackgroundTasks, data: dict = Body(..
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
